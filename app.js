f968e8b1fdc348f10688dad65ccfc49a501e4c89
henfredemars <henfredemars@users.noreply.github.com>
2015-01-16 20:49:08 -0500

    Added Hello World template

---
+++ app.js
@@ -0,0 +1,14 @@
+//Core app
+
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+var server = app.listen(3000, function () {
+
+  console.log('Listening for connections on port 3000');
+
+})
