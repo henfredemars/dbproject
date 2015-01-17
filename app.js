a9a24d1626d2d283dc9fd87bc2becb5bfea2b18c
henfredemars <henfredemars@users.noreply.github.com>
2015-01-16 20:55:17 -0500

    Added more structure, express now serves static content

--- app.js
+++ app.js
@@ -3,9 +3,12 @@
 var express = require('express')
 var app = express()
 
+//Routes
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+
+app.use(express.static(__dirname + '/public'));
 
 var server = app.listen(3000, function () {
 
