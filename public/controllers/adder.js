8e09c301fe229f76256c1aa20402d0df8d65df76
henfredemars <marsrover7@gmail.com>
2015-01-17 22:14:35 -0500

    More additions to the angular example

--- public/controllers/adder.js
+++ public/controllers/adder.js
@@ -3,9 +3,15 @@ var app = angular.module('app',[]);
 app.controller('adderController', function($scope) {
 	$scope.firstNumber = 2;
 	$scope.secondNumber = 2;
+	$scope.lvalue = 2+2;
 	$scope.thirdNumber = function () {
-		return parseInt($scope.firstNumber)+
+		if (!$scope.firstNumber || !$scope.secondNumber) {
+			return $scope.lvalue;
+		} else {
+		$scope.lvalue=parseInt($scope.firstNumber)+
 			parseInt($scope.secondNumber);
+			return $scope.lvalue;
+		}
 	};
 });
 
