2747dacc5fc6ce60a476a657f4c0ba130ca54bc3
henfredemars <henfredemars@users.noreply.github.com>
2015-02-01 01:23:06 -0500

    Changes to allow public access when not running with grunt

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 //Core app
 
 var express = require('express');
+var path = require('path');
 var app = express();
 
 //Routes
 app.get('/', function (req, res) {
   res.send('Hello World! This is an expressjs route!');
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.static(__dirname + '/public'));
 
