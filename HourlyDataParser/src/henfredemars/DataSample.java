06254c6dc6e367c324032718c935bdb6f5e0ca45
henfredemars <henfredemars@users.noreply.github.com>
2015-02-19 13:55:00 -0500

    Update DataSample.java
    Update header comment

--- HourlyDataParser/src/henfredemars/DataSample.java
+++ HourlyDataParser/src/henfredemars/DataSample.java
@@ -7,7 +7,7 @@ import java.util.Calendar;
 
 import org.junit.Test;
 
-//Stub
+//Single data point from one station at one hour
 public class DataSample implements DataSampleInterface, Serializable {
 	
 	private static final long serialVersionUID = 0L;
