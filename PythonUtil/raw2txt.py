4ad460f45375e36e742f7cee8ecfed39812d468a
henfredemars <henfredemars@users.noreply.github.com>
2015-02-26 03:44:39 -0500

    Bug fixes

--- PythonUtil/raw2txt.py
+++ PythonUtil/raw2txt.py
@@ -4,16 +4,23 @@ import os
 from time import sleep
 
 DATA_FOLDER = "../../ftp.ncdc.noaa.gov/pub/data/noaa"
+ISH_CLASSPATH = "../../ftp.ncdc.noaa.gov"
 
 def main():
   for sdir, dirs, files in os.walk(DATA_FOLDER):
     for file in files:
-      print("Processing {}".format(file))
-      print("Unzipping {}".format(file))
-      os.system('gunzip ' + os.path.join(sdir, file))
-      print("Formatting {} into table".format(file))
-      os.system('java ishJava {} {}'.format(file,file+'.txt'))
-      sleep(1)
+      if not '.gz' in file:
+        continue
+      ffile = os.path.join(sdir, file)
+      print("Processing {}...".format(file))
+      print("Unzipping {}...".format(file))
+      os.system('gunzip ' + ffile)
+      print("Formatting {} into table...".format(file))
+      os.system('java -classpath {} ishJava {} {}'.format(
+        ISH_CLASSPATH,ffile,ffile+'.txt'))
+      print("Re-compressing file...")
+      os.system('gzip --best {}'.format(ffile+'.txt'))
+      sleep(1) #Be nice
 
 if __name__=='__main__':
   print("I'm main!")
