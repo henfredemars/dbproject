a38f80919811b9ea4dc991accccef2ee22b8098e
henfredemars <henfredemars@users.noreply.github.com>
2015-03-05 23:22:49 -0500

    Update CompounderTool.java

--- HourlyDataParser/src/henfredemars/CompounderTool.java
+++ HourlyDataParser/src/henfredemars/CompounderTool.java
@@ -16,17 +16,21 @@ import java.util.zip.GZIPOutputStream;
 //Makes weekly summaries for each of the selected stations
 //climate_data,stations.dat,stations.txt,output
 public class CompounderTool {
-	public static final int stationIncrement = 35;
+	public static final int stationIncrement = 20;
 	
 	public static void main(String[] args) {
 		String[] allStations = readStations(args[1]).toArray(new String[0]);
 		ObjectOutputStream oos = prepareOutput(args[3]);
-		ArrayList<Station> compoundedStations = new ArrayList<Station>();
+		StationLocator sl = new StationLocator(args[2]);
 		int stationIndex = 0;
 		while (true) {
-			System.out.println("Processing stations " + stationIndex + " to " +
-					String.valueOf(stationIndex+stationIncrement-1) + "...");
 			if (stationIndex >= allStations.length) break;
+			int maxRange = stationIndex+stationIncrement-1;
+			if (maxRange > allStations.length-1) {
+				maxRange = allStations.length-1;
+			}
+			System.out.println("Processing stations " + stationIndex + " to " +
+					String.valueOf(maxRange) + "...");
 			ArrayList<String> curStationIds = new ArrayList<String>(stationIncrement);
 			ArrayList<DataSample> dataSamples = new ArrayList<DataSample>(stationIncrement);
 			for (int i = stationIndex; i<stationIndex+stationIncrement && i<allStations.length;i++) {
@@ -63,25 +67,29 @@ public class CompounderTool {
 			start.set(Calendar.HOUR_OF_DAY,0);
 			start.set(Calendar.MINUTE,0);
 			Calendar end = Calendar.getInstance();
-			start.set(Calendar.YEAR,2014);
-			start.set(Calendar.MONTH,Calendar.DECEMBER);
-			start.set(Calendar.DAY_OF_MONTH,31);
-			start.set(Calendar.HOUR_OF_DAY,23);
-			start.set(Calendar.MINUTE,59);
+			end.set(Calendar.YEAR,2014);
+			end.set(Calendar.MONTH,Calendar.DECEMBER);
+			end.set(Calendar.DAY_OF_MONTH,31);
+			end.set(Calendar.HOUR_OF_DAY,23);
+			end.set(Calendar.MINUTE,59);
 			wc.compoundDataByWeek(start, end);
-			compoundedStations.addAll(wc.getStations());
+			ArrayList<Station> compoundedStations = wc.getStations();
 			wc = null; //Free ASAP
-		}
-		StationLocator sl = new StationLocator(args[2]);
-		for (Station s: compoundedStations) {
-			float[] latlon = sl.getLatLon(s.getStationId());
-			s.setLatLon(latlon[0], latlon[1]);
-		}
-		try {
-			oos.writeObject(compoundedStations);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			for (Station s: compoundedStations) {
+				float[] latlon = sl.getLatLon(s.getStationId());
+				s.setLatLon(latlon[0],latlon[1]);
+				System.out.println("Writing " + s.getStationId() + ", " + s.getSamples().size() + " samples.");
+				try {
+					oos.writeObject(s);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			try {
+				oos.reset();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 		try {
 			oos.close();
@@ -132,7 +140,7 @@ public class CompounderTool {
 			e.printStackTrace();
 		}
 		try {
-			ArrayList<String> res = (ArrayList<String>) ois.readObject();
+			ArrayList<String> res = (ArrayList<String>)ois.readObject();
 			ois.close();
 			return res;
 		} catch (ClassNotFoundException | IOException e) {
